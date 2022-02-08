693aa39211d5b40b411feed169a442f79d5708e8
Manas Shukla <shuklamanasofficial@gmail.com>
2022-02-08 14:38:15 +0530

    Bug Fixes for Deployment

--- src/Components/Main/Home.jsx
+++ src/Components/Main/Home.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
-import HeaderLogo from '../../assets/greentransparent.png'
+import HeaderLogo from '../../assets/greentransparentbgimage.png'
 import FooterLogo from '../../assets/GreenbuiltPurple.png'
 import MobileScreen from '../../assets/mobile.png'
 import Coupon from '../../assets/price.png'
